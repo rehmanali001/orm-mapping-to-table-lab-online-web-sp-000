7ea570cc99f54057a33a11a4f040b106c5c888e7
Rehman Ali <rehmanali001@gmail.com>
2019-09-03 17:16:37 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
   attr_reader :id
   
   def initialize(name, grade, id=nil)
-    @name = name 
+    @name = name
     @grade = grade
     @id = id
   end
@@ -15,12 +15,28 @@ class Student
         name TEXT,
         grade TEXT
       )
+      SQL
       DB[:conn].execute(sql)
   end
   
   def self.drop_table
-  
-  
+    sql = <<-SQL
+    DROP TABLE students
+       SQL
+      DB[:conn].execute(sql)
   end
   
+  def save
+    sql = <<-SQL
+    INSERT INTO students (name, grade) VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+  
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
 end
